ab0b76e5f5303236b4c246edf946030414c33ec3
k0repan <i.m.korepan@gmail.com>
2025-10-10 17:16:20 +0300

    Add CMake config files and installed SFML.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(){
-    printf("This is Go game project\n");
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, Go!" << std::endl;
+
+    return 0;
 }
